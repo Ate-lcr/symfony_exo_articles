1875bf642e9d9128493493d8d4660a3ca5a60bc2
Antoine Leclaire <104761460+Ate-lcr@users.noreply.github.com>
2022-07-04 11:06:20 +0200

    second third symfony_exo_articles

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,8 +1,75 @@
 <?php
-
 namespace App\Controller;
 
-class PageController
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+class PageController extends AbstractController
 {
 
+    public array $articles = [
+            1 => [
+                'title' => 'Non, là c\'est sale',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet assumenda deserunt eius eveniet molestias necessitatibus non, quos sed sequi! Animi aspernatur assumenda earum laudantium odio quasi quibusdam quisquam veniam.',
+                'isPublished' => true,
+                'author' => 'Eric',
+                'image' => 'https://media.gqmagazine.fr/photos/5b991bbe21de720011925e1b/master/w_780,h_511,c_limit/la_tour_montparnasse_infernale_1893.jpeg'
+            ],
+            2 => [
+                'title' => 'Il faut trouver tous les gens qui étaient de dos hier',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet assumenda deserunt eius eveniet molestias necessitatibus non, quos sed sequi! Animi aspernatur assumenda earum laudantium odio quasi quibusdam quisquam veniam.',
+                'isPublished' => true,
+                'author' => 'Maurice',
+                'image' => 'https://fr.web.img6.acsta.net/r_1280_720/medias/nmedia/18/35/18/13/18369680.jpg'
+            ],
+            3 => [
+                'title' => 'Pluuutôôôôt Braaaaaach, Vasarelyyyyyy',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet assumenda deserunt eius eveniet molestias necessitatibus non, quos sed sequi! Animi aspernatur assumenda earum laudantium odio quasi quibusdam quisquam veniam.',
+                'isPublished' => true,
+                'author' => 'Didier',
+                'image' => 'https://media.gqmagazine.fr/photos/5eb02109566df9b15ae026f3/master/pass/n-3freres.jpg'
+            ],
+            4 => [
+                'title' => 'Quand on attaque l\'empire, l\'empire contre attaque',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam amet assumenda deserunt eius eveniet molestias necessitatibus non, quos sed sequi! Animi aspernatur assumenda earum laudantium odio quasi quibusdam quisquam veniam.',
+                'isPublished' => true,
+                'author' => 'Mbala',
+                'image' => 'https://fr.web.img2.acsta.net/newsv7/21/01/20/15/49/5077377.jpg'
+            ],
+        ];
+
+
+    /**
+     * @Route("/",name="home")
+     */
+    public function home(){
+        return $this->render('home.html.twig');
+    }
+
+
+    /**
+     * @Route("/list",name="list")
+     */
+    public function list(){
+        return $this->render('list.html.twig', [
+            "articles" => $this->articles,
+        ]);
+    }
+
+
+    /**
+     * @Route("list/article/{id}", name="list_article")
+     */
+    public function listArticle ($id){
+        $article=$this->articles[$id];
+
+        return $this->render('listarticle.html.twig', [
+            "article" => $article
+        ]);
+    }
 }
