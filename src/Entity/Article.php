7bae7faa320309a9d7ec1b83ab9b8f6953850014
Antoine Leclaire <104761460+Ate-lcr@users.noreply.github.com>
2022-07-06 12:49:18 +0200

    sixthteenth symfony_exo_articles

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -32,6 +32,11 @@ class Article
      */
     private $image;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -69,6 +74,18 @@ class Article
     public function setImage(?string $image): self
     {
         $this->image = $image;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
