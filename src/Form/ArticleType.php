bfc3824edb79750c321087496c6419070ceb13f5
Antoine Leclaire <104761460+Ate-lcr@users.noreply.github.com>
2022-07-12 12:25:44 +0200

    create articles with FK category symfony_exo_articles

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,10 +3,13 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class ArticleType extends AbstractType
 {
@@ -17,9 +20,10 @@ class ArticleType extends AbstractType
             ->add('image')
             ->add('author')
             ->add('content')
+//            J'ajoute la possibilité de pouvoir choisir la catégorie directement à la création de mon article avec EntityType
+            ->add('category', EntityType::class,['class' => Category::class,'choice_label' => 'title',])
             ->add('isPublished')
-            ->add('submit', SubmitType::class)
-        ;
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
